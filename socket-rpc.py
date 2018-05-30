005cde363c3cbb643165994006a80c0f79289c16
Ed Me <603803799@qq.com>
2018-05-30 15:00:38 +0800

    modify sockt

--- socket-rpc.py
+++ socket-rpc.py
@@ -1,11 +1,12 @@
 import socket
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('120.55.38.33', 5805))
 print('socket connected!')
 data = {
-    "jsonrpc" : "2.0",
-    "method" : "",
+    "jsonrpc" : 2.0,
+    "method" : None,
     "params" : None,
     "id" : 1
 }
@@ -13,7 +14,8 @@ data = {
 while True:
     data['method'] = input('rpc-method：')
     data['params'] = input('rpc-params：')
-    s.send('{} \r\n'.format(data).encode('utf-8'))
+
+    s.send(json.dumps(data).encode('utf-8') + b'\n')
 
     # 每次最多接收1k字节:
-    print(s.recv(1024))
+    print(s.recv(1024).decode('utf-8'))
